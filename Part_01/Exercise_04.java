b45a4534ac365f4b9839c7d0e2f838dcffde3206
Ryan Desmond <radesmond@gmail.com>
2017-03-15 17:15:04 +0800

    clarifying Exercise 4

--- Part_01/Exercise_04.java
+++ Part_01/Exercise_04.java
@@ -5,7 +5,7 @@ package Part_01;
  *      Write the necessary code to print the following table to the console:
  *
  *      a   a*2 a*4
- *      1   1   1
+ *      1   2   4
  *      ....
  *      4   8   16
  *
@@ -14,9 +14,9 @@ public class Exercise_04 {
 
      public static void main(String[] args) {
 
-         System.out.printf("%3s  |%5s  |%5s\n", "a", "a^2", "a^3");
+         System.out.println("a", "a*2", "a*4");
          for (int i = 1; i <= 4; i++) {
-         //    System.out.printf("%3d  |%5d  |%5d\n", ...);
+         //    System.out.printf(...);
          }
      }
 }
