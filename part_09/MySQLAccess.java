35c550e9485112c0ecd8db16b3ea1350c21ccdf7
Ryan Desmond <radesmond@gmail.com>
2017-04-03 13:59:39 +0800

    clean up

--- part_09/MySQLAccess.java
+++ part_09/MySQLAccess.java
@@ -1,14 +1,11 @@
 package part_09;
 
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
 
 public class MySQLAccess {
     private Connection connect = null;
