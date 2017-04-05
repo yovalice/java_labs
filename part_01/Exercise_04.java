e52ae288a9ed53133e027de5522efa3a4e882e9a
Yovalice Roman <yovalice@me.com>
2017-04-05 08:47:20 +0800

    fix

--- part_01/Exercise_04.java
+++ part_01/Exercise_04.java
@@ -1,23 +1,23 @@
-package Part_01;
-
- /**
- * Part 1 Exercise 4:
- *      Write the necessary code to print the following table to the console:
- *
- *      a   a*2 a*4
- *      1   2   4
- *      ....
- *      4   8   16
- *
- */
-public class Exercise_04 {
-
-     public static void main(String[] args) {
-
-         System.out.println("a", "a*2", "a*4");
-         for (int i = 1; i <= 4; i++) {
-         //    System.out.println(...);
-         }
-     }
-}
-
+//package Part_01;
+//
+// /**
+// * Part 1 Exercise 4:
+// *      Write the necessary code to print the following table to the console:
+// *
+// *      a   a*2 a*4
+// *      1   2   4
+// *      ....
+// *      4   8   16
+// *
+// */
+//public class Exercise_04 {
+//
+//     public static void main(String[] args) {
+//
+////         System.out.println("a", "a*2", "a*4");
+////         for (int i = 1; i <= 4; i++) {
+////         //    System.out.println(...);
+////         }
+//     }
+//}
+//
